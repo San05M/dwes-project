fadaf361301bd50e017459a3073b6e082d18e96c
San05M <san.mo.dra5@gmail.com>
2024-11-27 11:20:44 +0100

    routes.php update

---
+++ app/routes.php
@@ -0,0 +1,7 @@
+return [
+'' => 'app/controllers/index.php',
+'index' => 'app/controllers/index.php',
+'about' => 'app/controllers/about.php',
+'contact' => 'app/controllers/contact.php',
+'project' => 'app/controllers/project.php',
+];
